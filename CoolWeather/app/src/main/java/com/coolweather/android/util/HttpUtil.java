838b54570238ac87d6c1618258f743b95c93b03a
ITACHI <33789094+iiTachi@users.noreply.github.com>
2018-05-29 19:15:34 +0800

    Add files via upload
    aa

---
+++ CoolWeather/app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,8 @@
+public class HttpUtill{
+
+	public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url(address).build();
+		client.newCall(request).enqueue(callback);
+	}
+}
